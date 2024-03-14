a72a4dd45a3d2932b0625f53363dc93e2e6971d8
inmanturbo <47095624+inmanturbo@users.noreply.github.com>
2024-03-14 03:16:44 -0400

    Update app.php

--- stubs/one-app/bootstrap/app.php
+++ stubs/one-app/bootstrap/app.php
@@ -12,7 +12,8 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        //
+        //trust proxies
+        $middleware->trustProxies('*');
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
