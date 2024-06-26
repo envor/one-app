5ddfd30a36974231b30226a9c95fdd4bd0ed8e9a
inmanturbo <47095624+inmanturbo@users.noreply.github.com>
2024-06-26 10:37:02 -0400

    Update TeamController.php

--- stubs/navigation/app/Http/Controllers/TeamController.php
+++ stubs/navigation/app/Http/Controllers/TeamController.php
@@ -12,15 +12,20 @@ class TeamController extends Controller
     /**
      * Show the team management screen.
      *
+     * @param  \Illuminate\Http\Request  $request
      * @param  int  $teamId
      * @return \Illuminate\View\View
      */
     public function show(Request $request, $team)
     {
-        $team = Jetstream::newTeamModel()->where('uuid', $team)->firstOrFail();
+        $team = Jetstream::newTeamModel()->where('uuid',$team)->firstOrFail();
 
         if (Gate::denies('view', $team)) {
             abort(403);
+        }
+
+        if ($team->uuid != $request->user()->currentTeam->uuid) {
+            return redirect()->route('teams.show', $request->user()->currentTeam->uuid);
         }
 
         return view('teams.show', [
